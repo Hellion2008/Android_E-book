3a0611b8558bb50d8c3d70c41a38c1c57965320f
Hellion <semakin.s.v@gmail.com>
2024-10-19 01:42:28 +0300

    mini-app done

--- app/src/main/java/ru/urban/android_e_book/MainActivity.kt
+++ app/src/main/java/ru/urban/android_e_book/MainActivity.kt
@@ -1,12 +1,20 @@
 package ru.urban.android_e_book
 
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
+
+    private lateinit var toggleButtonTB: ToggleButton
+
+    private lateinit var textViewTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        textViewTV = findViewById(R.id.textView)
+        toggleButtonTB = findViewById(R.id.toggleButton)
+
+        toggleButtonTB.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCheckedChanged(view: CompoundButton?, isChecked: Boolean) {
+        if (isChecked){
+            val book = DataBase().loadBook("text")
+            for (line in book){
+                textViewTV.append(line + "\n")
+            }
+        } else{
+            textViewTV.setText("")
         }
     }
 }
